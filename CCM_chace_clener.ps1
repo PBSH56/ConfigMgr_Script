b605890b77e72b508b08484087de962ac0891bd6
PRABHAT KUMAR <42507130+PBSH56@users.noreply.github.com>
2025-07-23 00:07:59 +0530

    V1

---
+++ CCM_chace_clener.ps1
@@ -0,0 +1,16 @@
+CCM cache cklean
+
+$OldContent = (Get-ChildItem –Path "$Env:SystemDrive\Windows\ccmcache" -dir| Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-15))}).Name
+ForEach ($Folder in $OldContent)
+
+{
+Remove-Item -Path "$Env:SystemDrive\Windows\ccmcache\$Folder" -Recurse
+
+
+ If(!(Test-Path "$Env:SystemDrive\Windows\ccmcache\$Folder"))
+ {
+ Write-Output "$Folder is removed successfully"
+}
+
+
+}
